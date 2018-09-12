cf48cbb0c32d680832b9c758edc6e9f458a9be35
“Erin <erinkelley27@gmail.com>
2018-09-12 13:40:33 -0400

    added map marker

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
-import { withGoogleMap, GoogleMap } from 'react-google-maps'
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
 class Map extends Component {
   render () {
     const RecMap = withGoogleMap(props => (
-      <GoogleMap
-        defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
-        defaultZoom={13}
-      />
-    ))
+      <GoogleMap defaultCenter={{ lat: 40.756795, lng: -73.954298 }} defaultZoom={13}>
+        {props.isMarkerShown && <Marker position={{ lat: 40.756795, lng: -73.954298 }} />}
+      </GoogleMap>
+    )
+    )
     return (
       <div className='Map'>
         <RecMap
+          isMarkerShown
           containerElement={<div style={{ height: `500px`, width: '500px' }} />}
           mapElement={<div style={{ height: `100%` }} />}
         />
